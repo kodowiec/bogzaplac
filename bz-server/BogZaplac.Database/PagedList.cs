aa67c6a233f09986c092f15e630220ed01227233
kodowiec <62697517+kodowiec@users.noreply.github.com>
2021-12-13 19:26:20 +0100

    server.db: query historyitems

---
+++ bz-server/BogZaplac.Database/PagedList.cs
@@ -0,0 +1,24 @@
+﻿namespace BogZaplac.Database
+{
+    public class PagedList<T> : List<T>
+    {
+        public int Offset { get; set; }
+        public int Total { get; set; }
+        private IEnumerable<T> _list;
+
+        public PagedList(IEnumerable<T> items, int offset, int total) : base(items)
+        {
+            this.Offset = offset;
+            this.Total = total;
+            this._list = items;
+        }
+
+        public List<T> List
+        {
+            get
+            {
+                return this._list.ToList();
+            }
+        }
+    }
+}
---
+++ bz-server/BogZaplac.Database/Recepit.cs
@@ -0,0 +1,17 @@
+﻿namespace BogZaplac.Database
+{
+    public class Receipt
+    {
+        public int ID { get; set; }
+        public string Url { get; set; } = "";
+    }
+
+    public class HistoryItem
+    {
+        public int ID { get; set; }
+        public string Username { get; set; } = "";
+        public string Date { get; set; } = "";
+        public string Cost { get; set; } = "";
+        public int ReceiptID { get; set; }
+    }
+}
