a2eca76a413fdd7fc9db146c1e0899c600e5c638
kodowiec <62697517+kodowiec@users.noreply.github.com>
2021-12-13 20:14:22 +0100

    client: move screens to subdir

--- bz_client/lib/main.dart
+++ bz_client/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'titlescreen.dart';
+import 'screens/titlescreen.dart';
 import 'settings.dart';
 
 ConnectionSettings connectionSettings = ConnectionSettings();
