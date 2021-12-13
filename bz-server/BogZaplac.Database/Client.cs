db8f0a7b07ced66b0b76a4d1f6c16d889a159fb4
kodowiec <62697517+kodowiec@users.noreply.github.com>
2021-12-13 19:53:18 +0100

    server: simple database client

---
+++ bz-server/BogZaplac.Database/Client.cs
@@ -0,0 +1,56 @@
+﻿using System.Data;
+using System.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace BogZaplac.Database
+{
+    public partial class Client
+    {
+        private string _hostname;
+        private int _port;
+        private string _database;
+        private string _username;
+        private string _password;
+        private string _connectionString;
+
+        private MySqlConnection _connection;
+
+        public DbConnection Connection
+        {
+            get
+            {
+                CreateConnection();
+
+                return this._connection;
+            }
+        }
+
+        public Client(string hostname, int port, string database, string user, string password)
+        {
+            this._hostname = hostname;
+            this._port = port;
+            this._database = database;
+            this._username = user;
+            this._password = password;
+            this._connectionString = string.Format("server={0};uid={1};pwd={2};database={3};", hostname, user, password, database);
+
+            CreateConnection();
+        }
+
+        private void CreateConnection()
+        {
+            if (this._connection == null) this._connection = new MySqlConnection(this._connectionString);
+            if (this._connection.State != System.Data.ConnectionState.Open) this._connection.Open();
+        }
+
+        public void Close(bool dispose = false)
+        {
+            this._connection.Close();
+            if (dispose)
+            {
+                this._connection.Dispose();
+                this._connection = null;
+            }
+        }
+    }
+}
---
+++ bz-server/BogZaplac.Database/Queries.cs
@@ -0,0 +1,14 @@
+﻿using Dapper;
+
+namespace BogZaplac.Database
+{
+    partial class Client
+    {
+        public PagedList<HistoryItem> GetHistoryItems(int limit = 0, int offset = 0)
+        {
+            var total = Connection.Query<int>($"SELECT * FROM history ORDER BY id DESC").FirstOrDefault();
+            var result = Connection.Query<HistoryItem>($"SELECT * FROM history ORDER BY id DESC LIMIT {limit} OFFSET {offset}").AsList();
+            return new PagedList<HistoryItem>(result, offset, total);
+        }
+    }
+}
