b647b9293eae3aa14073fece84002319f7614506
kodowiec <62697517+kodowiec@users.noreply.github.com>
2021-12-14 01:16:05 +0100

    server.API: new endpoints
    - /history - POST - push new entry
    - /history/{id} - GET - get entry with id
    new controller: ReceiptController
    - /receipt - POST - push new receipt
    - /receipt/{id} - GET - get receipt with id

--- bz-server/BogZaplac.Api/Controllers/HistoryController.cs
+++ bz-server/BogZaplac.Api/Controllers/HistoryController.cs
@@ -15,12 +15,33 @@ namespace BogZaplac.Api.Controllers
             _logger = logger;
         }
 
+        [HttpPost]
+        public IActionResult Create([FromServices] BogZaplac.Database.Client dbClient, [FromForm] HIUpload upload)
+        {
+            var tmp = new Database.HistoryItemWithImage();
+            tmp.Cost = upload.Cost;
+            tmp.Date = (upload.Date == null)? DateTime.Now.ToString("dd MMM yyyy HH:mm:ss") : upload.Date;
+            tmp.Username = upload.Username;
+            tmp.Image = (upload.Image == null)? null : GetByteArrayFromImage.Get(upload.Image);
+            
+            var hi = (tmp.Image == null)? dbClient.PutHistoryItem(tmp) :  dbClient.PutHistoryWithImage(tmp);
+            return (hi == null)? ApiResponse.NotModified() : ApiResponse.Json(HttpStatusCode.Created, hi);
+        }
+
         [HttpGet]
         public IActionResult Get([FromServices] BogZaplac.Database.Client dbClient)
         {
             List<BogZaplac.Database.HistoryItem> historyItems = dbClient.GetHistoryItems(100, 0).List;
 
             return ApiResponse.Json(HttpStatusCode.OK, historyItems.OrderByDescending(x => x.ID).ToList());
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult Get([FromServices] BogZaplac.Database.Client dbClient, int id)
+        {
+            var item = dbClient.GetHistoryItem(id);
+            return (item == null || item?.Date == null)? ApiResponse.NotFound() : ApiResponse.Json(HttpStatusCode.OK, item);
         }
     }
 }
