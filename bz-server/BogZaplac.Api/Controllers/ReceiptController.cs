f85f25aab514b1396efbea758972998db2e67aa1
kodowiec <62697517+kodowiec@users.noreply.github.com>
2021-12-14 01:16:05 +0100

    server.API: new endpoints
    - /history - POST - push new entry
    - /history/{id} - GET - get entry with id
    new controller: ReceiptController
    - /receipt - POST - push new receipt
    - /receipt/{id} - GET - get receipt with id

---
+++ bz-server/BogZaplac.Api/Controllers/ReceiptController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BogZaplac.Api.Utils;
+using System.Net;
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BogZaplac.Api.Controllers
+{
+    [Route("receipt")]
+    [ApiController]
+    public class ReceiptController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get([FromServices] BogZaplac.Database.Client dbClient, int id)
+        {
+            var rec = dbClient.GetReceipt(id);
+            return (rec != null)? ApiResponse.Json(HttpStatusCode.OK, new Receipt(rec.ID, rec.Image)) : ApiResponse.NotFound();
+        }
+        
+        [HttpPost]
+        public IActionResult Post([FromServices] BogZaplac.Database.Client dbClient, [FromForm] ReceiptUpload upload)
+        {
+            var rec = dbClient.PutReceipt(new Database.Receipt(upload.Username, GetByteArrayFromImage.Get(upload.image)));
+            return (rec != 0)? ApiResponse.Json(HttpStatusCode.Created, rec) : ApiResponse.NotModified();
+        }
+    }
+}
--- bz-server/BogZaplac.Api/Receipt.cs
+++ bz-server/BogZaplac.Api/Receipt.cs
@@ -2,16 +2,38 @@ namespace BogZaplac.Api
 {
     public class Receipt
     {
-        public int ID { get; set; }
-        public string Url { get; set; } = "";
+        public int? ID { get; set; }
+        public byte[] image { get; set; }
+
+        public Receipt() { }
+
+        public Receipt(int? id, byte[] img)
+        {
+            this.ID = id;
+            this.image = img;
+        }
+    }
+
+    public class ReceiptUpload
+    {
+        public string Username { get; set; } = "";
+        public IFormFile image { get; set; }
     }
 
     public class HistoryItem
     {
         public int ID { get; set; }
         public string Username { get; set; } = "";
-        public long Date { get; set; }
+        public string Date { get; set; } = "";
         public string Cost { get; set; } = "";
         public int ReceiptID { get; set; }
+    }
+
+    public class HIUpload
+    {
+        public string Username { get; set; }
+        public string Cost { get; set; }
+        public IFormFile? Image { get; set; }
+        public string? Date { get; set; }
     }
 }
