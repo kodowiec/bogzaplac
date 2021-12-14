f371dd4916f8380e71cdbb55fc9db0ea337ea972
kodowiec <62697517+kodowiec@users.noreply.github.com>
2021-12-14 01:10:29 +0100

    server.db:  add more sql queries
    - get one item from history
    - put item to history
    - put item to history with image
    - put receipt (image)
    - get receipt

--- bz-server/BogZaplac.Database/Queries.cs
+++ bz-server/BogZaplac.Database/Queries.cs
@@ -10,5 +10,32 @@ namespace BogZaplac.Database
             var result = Connection.Query<HistoryItem>($"SELECT * FROM history ORDER BY id DESC LIMIT {limit} OFFSET {offset}").AsList();
             return new PagedList<HistoryItem>(result, offset, total);
         }
+
+        public HistoryItem? GetHistoryItem(int id)
+        {
+            return Connection.Query<HistoryItem>($"SELECT * FROM history WHERE ID = {id}").FirstOrDefault();
+        }
+
+        public uint? PutHistoryWithImage(HistoryItemWithImage item)
+        {
+            var receipt = PutReceipt(new Receipt(item.Username, item.Image));
+            item.ReceiptID = (int?) receipt;
+            return (receipt == 0)? null : PutHistoryItem(item);
+        }
+
+        public uint PutHistoryItem(HistoryItem item)
+        {
+            return Connection.Query<uint>("INSERT INTO history (Username, Cost, Date, ReceiptID) VALUES (@Username, @Cost, @Date, @ReceiptID); select LAST_INSERT_ID();", item).First();
+        }
+
+        public Receipt? GetReceipt(int id)
+        {
+            return Connection.Query<Receipt>($"SELECT * FROM receipts WHERE ID={id}").FirstOrDefault();
+        }
+
+        public uint PutReceipt(Receipt receipt)
+        {
+            return Connection.Query<uint>("INSERT INTO receipts (Username, Image) VALUES (@Username, @Image); select LAST_INSERT_ID();", receipt).First();
+        }
     }
 }
--- bz-server/BogZaplac.Database/Recepit.cs
+++ bz-server/BogZaplac.Database/Recepit.cs
@@ -2,16 +2,30 @@
 {
     public class Receipt
     {
-        public int ID { get; set; }
-        public string Url { get; set; } = "";
+        public int? ID { get; set; }
+        public string Username { get; set; }
+        public byte[] Image { get; set; }
+
+        public Receipt() { }
+
+        public Receipt(string user, byte[] img)
+        {
+            this.Username = user;
+            this.Image = img;
+        }
     }
 
     public class HistoryItem
     {
-        public int ID { get; set; }
+        public int? ID { get; set; }
         public string Username { get; set; } = "";
         public string Date { get; set; } = "";
         public string Cost { get; set; } = "";
-        public int ReceiptID { get; set; }
+        public int? ReceiptID { get; set; }
+    }
+
+    public class HistoryItemWithImage : HistoryItem
+    {
+        public byte[]? Image;
     }
 }
