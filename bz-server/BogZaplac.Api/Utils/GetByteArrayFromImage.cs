4fca1f96f0906c1180ede900b6003082d6ecb957
kodowiec <62697517+kodowiec@users.noreply.github.com>
2021-12-14 01:13:40 +0100

    server.API.utils: new resp. methods and image->byte[] tool

---
+++ bz-server/BogZaplac.Api/Utils/GetByteArrayFromImage.cs
@@ -0,0 +1,14 @@
+﻿namespace BogZaplac.Api.Utils
+{
+    public class GetByteArrayFromImage
+    {
+        public static byte[] Get(IFormFile file)
+        {
+            using (var target = new MemoryStream())
+            {
+                file.CopyTo(target);
+                return target.ToArray();
+            }
+        }
+    }
+}
