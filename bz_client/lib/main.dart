58c303acb06061ca165e6e72e2a2c365854569fb
kodowiec <62697517+kodowiec@users.noreply.github.com>
2021-12-17 04:56:44 +0100

    client: hook shared_preferences package
    im so sorry for this i wrote this at 4 am

--- bz_client/lib/main.dart
+++ bz_client/lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'screens/titlescreen.dart';
 import 'classes/settings.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 late UserSettings userSettings;
-void main() => runApp(const MyApp());
+late SharedPreferences prefs;
+void main() async
+{
+  // i think this is kinda dirty way of handling this
+  WidgetsFlutterBinding.ensureInitialized();
+  prefs = await SharedPreferences.getInstance();
+  runApp(const MyApp());
+}
 
 void initSettings()
 {
   userSettings = UserSettings(username: "err_def_username", connection: ConnectionSettings());
+  userSettings.username           = prefs.getString("username")   ?? "err_def_username";
+  userSettings.connection.host    = prefs.getString("hostname")   ?? "localhost";
+  userSettings.connection.port    = prefs.getString("port")       ?? "2137";
+  userSettings.connection.isHttps = prefs.getBool("serverHttps")  ?? false;
+  userSettings.isFirstRun         = prefs.getBool("isFirstRun")   ?? true;
   userSettings.connection.buildUrl();
 }
 
